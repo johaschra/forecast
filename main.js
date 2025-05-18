c7cde778e8d3c2ce2db7bda27afe25cd6e011ec8
Johanna Schramm <johanna.schramm@student.uibk.ac.at>
2025-05-18 16:53:07 +0200

    funktioniert nicht ;(

--- main.js
+++ main.js
@@ -99,7 +99,64 @@ async function loadWind(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     console.log(jsondata);
+    return jsondata;
    
 }
 
-loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json")
+
+
+
+async function WindLayer() {
+    //let winddata = await loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json")
+    let winddata = await loadWind("demo.json");
+    
+    
+    console.log(winddata);
+    
+
+
+    var velocityLayer = L.velocityLayer({
+    displayValues: true,
+    displayOptions: {
+        // label prefix
+        velocityType: "Global Wind",
+
+        // leaflet control position
+        position: "bottomleft",
+
+        // no data at cursor
+        emptyString: "No velocity data",
+
+        // see explanation below
+        angleConvention: "bearingCW",
+
+        // display cardinal direction alongside degrees
+        showCardinal: false,
+
+        // one of: ['ms', 'k/h', 'mph', 'kt']
+        speedUnit: "ms",
+
+        // direction label prefix
+        directionString: "Direction",
+
+        // speed label prefix
+        speedString: "Speed",
+    },
+    data: winddata, // see demo/*.json, or wind-js-server for example data service
+
+    // OPTIONAL
+    minVelocity: 0, // used to align color scale
+    maxVelocity: 10, // used to align color scale
+    velocityScale: 0.005, // modifier for particle animations, arbitrarily defaults to 0.005
+    colorScale: [], // define your own array of hex/rgb colors
+    onAdd: null, // callback function
+    onRemove: null, // callback function
+    opacity: 0.97, // layer opacity, default 0.97
+
+    // optional pane to add the layer, will be created if doesn't exist
+    // leaflet v1+ only (falls back to overlayPane for < v1)
+    paneName: "overlayPane",
+    }).addTo(overlays.wind);
+}
+
+WindLayer();
